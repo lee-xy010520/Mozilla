da27a8f2b8d02ec8823b4c6fbd96b1ae48ec4751
lee-xy010520 <125971056+lee-xy010520@users.noreply.github.com>
2023-02-22 15:20:19 +0800

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,38 @@
+//let myHeading = document.querySelector('h1');
+//myHeading.textContent = 'Hello world!';
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/Firefox.png') {
+      myImage.setAttribute('src', 'images/3.png');
+    } else {
+      myImage.setAttribute('src', 'images/Firefox.png');
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+function setUserName() {
+  let myName = prompt('请输入你的名字。');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Mozilla 酷毙了，' + myName;
+}
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla 酷毙了，' + storedName;
+}
+myButton.onclick = function() {
+   setUserName();
+}
+function setUserName() {
+  let myName = prompt('请输入你的名字。');
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla 酷毙了，' + myName;
+  }
+}
